32f7c4e4f01f1a0f5794611a92c0f6e2a2a201c8
Chiedozie Ikeanyi <ikeanyi.chiedozie@gmail.com>
2023-02-28 20:28:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import Public from "./components/Public";
 import Login from "./features/auth/Login";
 import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="Login" element={<Login />} />
-        <Route path="dash" element={<DashLayout />}></Route>
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+        </Route>
       </Route>
     </Routes>
   );
